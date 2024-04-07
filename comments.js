62f348caebec2b9642b1984a9ece49887d58a157
fatma <141265875+fatmasoly@users.noreply.github.com>
2024-04-07 12:07:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,42 +1,73 @@
 // create web server 
-// load express module
-const express = require('express');
-// create web server
-const app = express();
-// load body-parser module
-const bodyParser = require('body-parser');
-// load comments module
-const comments = require('./comments');
-// load cors module
-const cors = require('cors');
-// use cors
-app.use(cors());
-// use body-parser
-app.use(bodyParser.json());
-// get all comments
-app.get('/comments', (req, res) => {
-    res.json(comments.getComments());
+// import http module
+const http = require('http');
+// import fs module
+const fs = require('fs');
+// import url module
+const url = require('url');
+
+// create server
+http.createServer((req, res) => {
+
+    const pathName = url.parse(req.url, true).pathname;
+    const query = url.parse(req.url, true).query;
+    // get file path
+    const filePath = `./data/${query.id}.json`;
+    // get file content
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    // get file content to json
+    const comments = JSON.parse(fileContent);
+    // get comments
+    let commentsStr = '';
+    comments.forEach((comment) => {
+        commentsStr += `${comment.name}: ${comment.content}<br>`;
+    });
+
+    // if the request url is /getComments
+    if (pathName === '/getComments') {
+        // response
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(commentsStr);
+    } else {
+        // if the request url is not /getComments
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+        res.end('404 Not Found');
+    }
+
+}).listen(3000, () => {
+    console.log('Server is running...');
 });
-// add a new comment
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comments.addComment(comment);
-    res.json(comment);
-});
-// delete a comment
-app.delete('/comments/:id', (req, res) => {
-    const id = req.params.id;
-    comments.deleteComment(id);
-    res.json(id);
-});
-// update a comment
-app.put('/comments/:id', (req, res) => {
-    const id = req.params.id;
-    const comment = req.body;
-    comments.updateComment(id, comment);
-    res.json(comment);
-});
-// start web server
-app.listen(3000, () => {
-    console.log('Web server is up and running');
-});
+
+// run the server
+// node comments.js
+// open browser and enter http://localhost:3000/getComments?id=1
+// open browser and enter http://localhost:3000/getComments?id=2
+
+// Path: comments.js
+// create web server 
+// import http module
+const http = require('http');
+// import fs module
+const fs = require('fs');
+// import url module
+const url = require('url');
+
+// create server
+http.createServer((req, res) => {
+
+    const pathName = url.parse(req.url, true).pathname;
+    const query = url.parse(req.url, true).query;
+    // get file path
+    const filePath = `./data/${query.id}.json`;
+    // get file content
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    // get file content to json
+    const comments = JSON.parse(fileContent);
+    // get comments
+    let commentsStr = '';
+    comments.forEach((comment) => {
+        commentsStr += `${comment.name}: ${comment.content}<br>`;
