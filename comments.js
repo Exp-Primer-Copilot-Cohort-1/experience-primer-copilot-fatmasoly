e09969de64752e3a16ac54cc7765398a13bd4ba0
fatma <141265875+fatmasoly@users.noreply.github.com>
2024-04-07 12:13:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -41,33 +41,3 @@ http.createServer((req, res) => {
 }).listen(3000, () => {
     console.log('Server is running...');
 });
-
-// run the server
-// node comments.js
-// open browser and enter http://localhost:3000/getComments?id=1
-// open browser and enter http://localhost:3000/getComments?id=2
-
-// Path: comments.js
-// create web server 
-// import http module
-const http = require('http');
-// import fs module
-const fs = require('fs');
-// import url module
-const url = require('url');
-
-// create server
-http.createServer((req, res) => {
-
-    const pathName = url.parse(req.url, true).pathname;
-    const query = url.parse(req.url, true).query;
-    // get file path
-    const filePath = `./data/${query.id}.json`;
-    // get file content
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    // get file content to json
-    const comments = JSON.parse(fileContent);
-    // get comments
-    let commentsStr = '';
-    comments.forEach((comment) => {
-        commentsStr += `${comment.name}: ${comment.content}<br>`;
